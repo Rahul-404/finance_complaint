ad5d9c4e308861dd67bfa762e7222387ad784aa9
Rahul-404 <rahulshelke981@gmail.com>
2025-01-19 09:17:08 +0530

    added logger exception utils common code

---
+++ finance_complaint/utils.py
@@ -0,0 +1,30 @@
+from logger import logging
+from exception import FinanceException
+import yaml
+import sys
+import os
+
+def write_yaml_file(file_path: str, data: dict = None):
+    """ 
+    Create a yaml file
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as yaml_file:
+            if data is not None:
+                yaml.dump(data, yaml_file)
+    except Exception as e:
+        raise FinanceException(e, sys)
+
+def read_yaml_file(file_path: str) -> dict:
+    """ 
+    Reads a YAML file and returns the concepts as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise FinanceException(e, sys)
