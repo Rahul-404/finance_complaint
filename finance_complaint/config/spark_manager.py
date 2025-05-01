ddf79a96783110015dcc299980edc5ed0bea39cc
Rahul-404 <rahulshelke981@gmail.com>
2025-05-02 01:50:58 +0530

    Update Spark session configuration for improved resource allocation

--- finance_complaint/config/spark_manager.py
+++ finance_complaint/config/spark_manager.py
@@ -1,8 +1,21 @@
 from pyspark.sql import SparkSession
+from dotenv import load_dotenv
+import os
 
-spark_session = (SparkSession.builder.master('local[*]').appName('finance_complaint')\
-    .config("spark.executor.instances", "1")\
-    .config("spark.executor.memory", "6g")\
-    .config("spark.driver.memory", "6g")\
-    .config("spark.executor.memoryOverhead", "8g")\
+load_dotenv()
+
+# spark_session = (SparkSession.builder.master('local[*]')
+# .config("spark.executor.memory","4g")
+# .config("spark.executor.cores","2")
+# .config("spark.files.maxPartitionBytes","134217728")
+# .appName('finance_complaint').getOrCreate())
+
+
+
+
+spark_session = (SparkSession.builder.master('local[*]').appName('finance_complaint')
+    .config("spark.executor.instances", "2")
+    .config("spark.executor.memory", "8g")
+    .config("spark.driver.memory", "8g")
+    .config("spark.executor.memoryOverhead", "8g")
     .getOrCreate())
