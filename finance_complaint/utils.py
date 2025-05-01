c49104be5052951afa21f37852fcf1e8500eafe6
Rahul-404 <rahulshelke981@gmail.com>
2025-05-02 03:15:51 +0530

    Add get_score function for evaluating model performance using MulticlassClassificationEvaluator

--- finance_complaint/utils.py
+++ finance_complaint/utils.py
@@ -3,6 +3,9 @@ from .logger import logging as logger
 import yaml
 import sys
 import os
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.sql import DataFrame
+from finance_complaint.logger import logging as logger
 
 def write_yaml_file(file_path: str, data: dict = None):
     """ 
@@ -26,5 +29,19 @@ def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise FinanceException(e, sys)
+    
+def get_score(dataframe: DataFrame, metric_name, label_col, prediction_col) -> float:
+    try:
+        evaluator = MulticlassClassificationEvaluator(
+            labelCol=label_col,
+            predictionCol=prediction_col,
+            metricName=metric_name
+        )
+        score = evaluator.evaluate(dataframe)
+        print(f"{metric_name} score: {score}")
+        logger.info(f"{metric_name} score: {score}")
+        return score
     except Exception as e:
         raise FinanceException(e, sys)
